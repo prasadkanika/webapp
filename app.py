06c730135d27968cd66e5c00a71fb31c00e5478f
prasadkanika <48556804+prasadkanika@users.noreply.github.com>
2022-03-30 11:44:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,38 +10,18 @@ import dash_core_components as dcc
 import dash_html_components as html
 import base64
 
-wheels = pd.read_csv("wheels.csv")
 
-def encode_img(img):
-    encoded = base64.b64encode(open(img,'rb').read())
-    return f"data:image/png;base64,{encoded.decode()}"
+  markdown= '''\n
+ 
+ [my meat website](https://meatweb.s3.ap-south-1.amazonaws.com/meat/index.html) \n
+ [my crypto website](https://mycryptoweb.s3.ap-south-1.amazonaws.com/arsha/index.html)
+ 
+'''
 
-options = [{'label':k , 'value':k} for k in wheels['wheels'].unique()]
-options1 = [{'label':k , 'value':k} for k in wheels['color'].unique()]
-
+app =dash.Dash()
 app.layout = html.Div([
-    dcc.RadioItems(id='wheels' , options=options , value=1) ,
-    html.Div(id='wheelsop'),
-    html.Hr() , #horizontal row line
-    dcc.RadioItems(id='colors' , options=options1 , value='blue') ,
-    html.Div(id='colorsop'),
-    html.Img(id='img' , src='children' , height=300)
-],style={'padding':'15px','font-size':'18px' ,'font-family':'helvetica'})
-
-@app.callback(Output('wheelsop','children') , [Input('wheels','value')])
-def callback_a(wheels_val):
-    return f'you choose {wheels_val}'
-
-@app.callback(Output('colorsop','children') , [Input('colors','value')])
-def callback_b(colors_val):
-    return f"you choose {colors_val}"
-
-@app.callback(Output('img','src') , [ Input('wheels','value') , Input('colors','value')])
-def image(wheel,color):
-    path='/Users/guruprasadkanika/Downloads/datascience/Plotly-Dashboards-with-Dash-master/Data/Images/'
-    img_df = wheels[ (wheels['wheels']==wheel) & (wheels['color']==color) ]
-    return encode_img(path+ img_df['image'].values[0] )
-    
+    dcc.Markdown(children=markdown)
+])
 
 if __name__=="__main__":
     app.run_server()
