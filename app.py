7c2ef76e4743e0161bec589ff5de9b09ef788228
prasadkanika <48556804+prasadkanika@users.noreply.github.com>
2022-03-31 11:38:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,34 +6,72 @@ import plotly.graph_objs as go
 import plotly.figure_factory as ff
 from plotly import tools
 import dash
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output , State
 import base64
+from datetime import datetime
+from datetime import timedelta
 
 
 app =Dash(__name__)
 server = app.server
 #======================
-dff = pd.read_csv("gapminderDataFiveYear.csv")
-year_options = []
-for year in dff['year'].unique():
-    year_options.append({'label':str(year) , 'value':year})
+
+df = pd.read_csv("Select.csv")
+df.set_index(keys='Issuer Name', inplace=True)
+
+options=[]
+for s in df.index:
+    di={}
+    di['label']=df.loc[s].get(0)
+    di['value']=s
+    options.append(di)
+options
 
 
+    
+mi= datetime.today() - timedelta(days=730)
+ma= datetime.today() 
+sd,ed=str(datetime.today()-timedelta(days=60))[:10] , str(datetime.today())[:10]
+    
 app.layout = html.Div([
-    dcc.Graph(id='graph') , 
-    dcc.Dropdown(id='year-picker' , options=year_options , value=dff['year'].min())
+    
+    html.H1("Stock ticker") ,
+    html.Div([
+         html.H3("Enter stock symbol" , style={'paddingRight':'30px'}) ,
+    dcc.Dropdown(id='stock_name' , options=options, value=['TITAN'],multi=True )]
+        , style={'display':'inline-block' , 'verticalAlign':'top','width':"30%"}) ,
+    
+    
+     html.Div([
+         html.H3("Select date range" ) ,
+         
+    dcc.DatePickerRange(id='date_range' ,min_date_allowed =mi ,max_date_allowed = ma ,start_date =sd, end_date=ed)
+        
+    ], style={'display':'inline-block'}) ,
+    
+    html.Div([
+        html.Button(id='submit_btn' , n_clicks=0 , children='Submit' , style={'fontSize':24 , 'marginLeft':"30px"})
+    ], style={'display':'inline-block'}) ,
+    
+    dcc.Graph(id='graph' , figure={
+        'data':[{'x':[1,2] , 'y':[3,1]}] , 'layout':{'title':'default title'}
+    }),
+    
 ])
 
-@app.callback(Output('graph','figure') , [Input('year-picker' , 'value')])
-#graph - id , property = graph for output and for input id is year-picker and property value
-def update_figure(selected_year):
-    filtered_df = dff[ dff['year']== selected_year]
-    traces =[]
-    for continent in filtered_df['continent'].unique():
-        df_by_continents = filtered_df[filtered_df['continent']==continent]
-        traces.append( go.Scatter(x=df_by_continents['gdpPercap'] , y=df_by_continents['lifeExp'] , mode='markers' , opacity=0.7 , marker={'size':15},name=continent) )
+@app.callback(Output('graph','figure'),[Input('submit_btn','n_clicks')],[State('stock_name','value') ,
+                                        State('date_range','start_date') , State('date_range','end_date')])
+
+def update_graph(n_clicks , stock_name , start_date , end_date):
+#    start_date = str(mi)[:10]
+#    end_date =str(ma)[:10]
+    traces=[]
+    for k in stock_name:
+        df = web.DataReader(name=k+".BO"  , data_source='yahoo', start = start_date ,end = end_date)
+        traces.append({'x':df.index , 'y':df['Close'], 'name':k})
     
-    return {'data':traces , 'layout':go.Layout(title='My plot' , xaxis={'title':'GDP per Capita','type':'log'} , yaxis={'title':'Life Expectancy'})}
+    fig = {'data':traces , 'layout':{'title': stock_name }}
+    return fig
 
 #===================================
 
