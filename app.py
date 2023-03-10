0f43e182271d7f7c7b32170ce9ee32dac9726973
prasadkanika <48556804+prasadkanika@users.noreply.github.com>
2023-03-10 11:15:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output, State
 import pandas as pd
 import pandas_datareader.data as web
