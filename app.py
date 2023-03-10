13d4ab866a1e42d11473de420de7759ec1712738
prasadkanika <48556804+prasadkanika@users.noreply.github.com>
2023-03-10 10:45:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,65 @@
-import numpy as np
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+import pandas as pd
+import pandas_datareader.data as web
+from datetime import datetime, timedelta
+
+app = dash.Dash(__name__)
+server = app.server
+
+df = pd.read_csv("new.csv")
+df.set_index(keys='Issuer Name', inplace=True)
+
+options = []
+for s in df.index:
+    di = {}
+    di['label'] = df.loc[s].get(2)
+    di['value'] = s
+    options.append(di)
+
+mi = datetime.today() - timedelta(days=730)
+ma = datetime.today()
+sd, ed = str(datetime.today()-timedelta(days=60))[:10], str(datetime.today())[:10]
+
+app.layout = html.Div([
+    html.H1("Stock ticker"),
+    html.Div([
+        html.H3("Enter stock symbol", style={'paddingRight': '30px'}),
+        dcc.Dropdown(id='stock_name', options=options, value=['TITAN'], multi=True)
+    ], style={'display': 'inline-block', 'verticalAlign': 'top', 'width': "30%"}),
+    html.Div([
+        html.H3("Select date range"),
+
+        dcc.DatePickerRange(id='date_range', min_date_allowed=mi, max_date_allowed=ma, start_date=sd, end_date=ed)
+
+    ], style={'display': 'inline-block'}),
+
+    html.Div([
+        html.Button(id='submit_btn', n_clicks=0, children='Submit', style={'fontSize': 24, 'marginLeft': "30px"})
+    ], style={'display': 'inline-block'}),
+
+    dcc.Graph(id='graph', figure={
+        'data': [{'x': [1, 2], 'y': [3, 1]}], 'layout': {'title': 'default title'}
+    }),
+
+])
+
+@app.callback(Output('graph', 'figure'), [Input('submit_btn', 'n_clicks')], [State('stock_name', 'value'),
+                                                                             State('date_range', 'start_date'),
+                                                                             State('date_range', 'end_date')])
+def update_graph(n_clicks, stock_name, start_date, end_date):
+    traces = []
+    for k in stock_name:
+        df = web.DataReader(name=k+".BO", data_source='yahoo', start=start_date, end=end_date)
+        traces.append({'x': df.index, 'y': df['Close'], 'name': k})
+
+    fig = {'data': traces, 'layout': {'title': stock_name}}
+    return fig
+
+'''import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.offline as pyo
@@ -73,6 +134,7 @@ def update_graph(n_clicks , stock_name , start_date , end_date):
     
     fig = {'data':traces , 'layout':{'title': stock_name }}
     return fig
+'''
 
 #===================================
 
